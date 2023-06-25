d4e8f713949c38395dd3c02a67c59e8f300d3df7
Guilherme Leal <guilhermelml@gmail.com>
2023-06-25 01:20:28 -0300

    add configs

---
+++ main.js
@@ -0,0 +1,26 @@
+//imports
+import express from "express";
+import cors from "cors";
+
+//iniciar express
+const app = express();
+
+//middlewares
+app.use(cors());
+
+
+app.get("/", (req, res) => {
+  console.log("get /");
+});
+
+// crud
+
+// create
+
+// read
+
+//update
+
+app.listen(8080, () => {
+  console.log("Server running on port 8080");
+});
