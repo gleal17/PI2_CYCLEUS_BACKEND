13d172172187813d96556dce81e32d908a486fcb
Guilherme Leal <guilhermelml@gmail.com>
2023-07-14 12:37:42 -0300

    resolvendo conflitos e add rotas

--- src/routes.ts
+++ src/routes.ts
@@ -7,11 +7,21 @@ const router = express.Router();
 const userController = new UserController();
 const lockController = new LockController();
 
+
+//users
 router.post('/create', userController.createUser);
 router.get('/users', userController.getAllUsers);
+router.get('/users/:id', userController.getUserByMatricula);
+
+
+//locks
+router.get('/stations', lockController.getStations);
+router.get('/locks/:id', lockController.getLockById);
+router.post('/locks', lockController.createLock);
+router.patch('/locks/:id', lockController.updateLock);
+
 
 // Não existem ainda:
-// router.get('/users/:id', userController.getUser);
 // router.post('register', userController.registerUser);
 // router.get('/stations', lockController.getStations);
 // router.post('/stations', lockController.createStation);
